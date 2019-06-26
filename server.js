45625202b798e8b33a92c8528f3bdefe5f618ec5
Ravi Kumar <ravikumar260995@gmail.com>
2019-06-26 11:39:29 +0530

    make it better

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express()
 const bodyParser = require("body-parser")
 const cors = require('cors')
 const path = require('path');
+var mongoose = require("mongoose")
 
 //step 1
 var port = process.env.PORT || 5000
-var mongoose = require("mongoose")
+
 
 
 app.use(bodyParser.json())
@@ -21,25 +22,24 @@ app.use(
     })
 )
 
+app.use('/users', Users)
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/collection'
-
-mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/collection', { useNewUrlParser: true })
     .then(() => console.log("Connected With MongoDB...!"))
     .catch(err => console.log(err))
 
 var Users = require('./routes/Users')
-app.use('/users', Users)
+
 
 
 
 //step 2
-if (process.env.NODE_ENV === 'production'){
-    
+if (process.env.NODE_ENV === 'production') {
+
     app.use(express.static('collection/build'));
 
-    app.get('*', (req,res)=> {
-        res.sendFile(path.join(__dirname,'collection','build','index.html')); // relative path
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'collection', 'build', 'index.html')); // relative path
     });
 }
 
@@ -53,6 +53,7 @@ app.listen(port, () => {
 
 
 
+app.use('/charts', chartRoutes);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/chartdb', { useNewUrlParser: true });
 const connection = mongoose.connection;
@@ -66,11 +67,11 @@ let Chart = require('./chartModel');
 
 const chartRoutes = express.Router();
 
-app.use('/charts', chartRoutes);
+
 
 chartRoutes.route('/').get(function (req, res) {
     Chart.find(function (err, charts) {
-        if (err) { 
+        if (err) {
         }
         else {
             res.json(charts);
@@ -78,32 +79,32 @@ chartRoutes.route('/').get(function (req, res) {
     });
 });
 
-chartRoutes.route('/:id').get(function(req, res) {
+chartRoutes.route('/:id').get(function (req, res) {
     let id = req.params.id;
-    Chart.findById(id, function(err, chart) {
+    Chart.findById(id, function (err, chart) {
         res.json(chart);
     });
 });
 
-chartRoutes.route('/add').post(function(req, res) {
+chartRoutes.route('/add').post(function (req, res) {
     let chart = new Chart(req.body);
     chart.save()
         .then(chart => {
-            res.status(200).json({'chart': 'chart details added'});
+            res.status(200).json({ 'chart': 'chart details added' });
         })
         .catch(err => {
             res.status(400).send('adding details failed');
         });
 });
 
-chartRoutes.route('/delete/:id').post(function(req,res){
-    Chart .findById(req.params.id,function(err,chart){
-        chart.remove({_id:req.body.id},function(err){
-            if(err){
+chartRoutes.route('/delete/:id').post(function (req, res) {
+    Chart.findById(req.params.id, function (err, chart) {
+        chart.remove({ _id: req.body.id }, function (err) {
+            if (err) {
                 res.send(err);
             }
-            else{
-                res.send({data: "Details Deleted"})
+            else {
+                res.send({ data: "Details Deleted" })
             }
         });
     });
